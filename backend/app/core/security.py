0ed2c8d3c5b22a4a73933f2356b20ad201003c75
daulet <zhakaevdaulet0709@gmail.com>
2025-12-02 06:59:56 +0500

    fix security

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -10,7 +10,7 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
 
-def get_password_hash(password: str) -> str:
+def hash_password(password: str) -> str:
     """Хеширует пароль (именно этой функции не хватало)"""
     return pwd_context.hash(password)
 
